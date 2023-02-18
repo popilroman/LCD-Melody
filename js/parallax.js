949d395abb9c75bfea232013283d0a2a443e7851
Popil Roman <popil.roman23@mail.ru>
2023-02-18 19:54:14 +0500

    Реализовал параллакс эффект

---
+++ js/parallax.js
@@ -0,0 +1,15 @@
+// Функция, которая проверяет загрузку HTML-документа и DOM-дерева
+document.addEventListener("DOMContentLoaded", function() {
+    // Функция, которая проверяет загрузку всего контента страницы
+    window.addEventListener("load", function() {
+
+        this.document.addEventListener("mousemove", parallax);
+
+        function parallax(event) {
+            const layer = document.querySelector(".main__parallax");
+            let speedLayer = layer.getAttribute('data-speed');
+            layer.style.transform = `translateX(${event.clientX * speedLayer / 2000}px) translateY(${event.clientY * speedLayer / 3000}px)`;
+        };
+
+     });
+});
