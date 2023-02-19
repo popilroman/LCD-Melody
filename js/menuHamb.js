e8309ca6100c43453d05600266eb707a2cb16dab
Popil Roman <popil.roman23@mail.ru>
2023-02-19 16:32:20 +0500

    Пофиксил баги

--- js/menuHamb.js
+++ js/menuHamb.js
@@ -7,9 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
         const menuSpan = this.document.querySelector(".hamb");
         const menuPanel = this.document.querySelector(".menu__panel");
 
-        menuHamb.addEventListener("click", function() {
-            transformBtn();
-            isClick();
+
+        //Массив для события по одному из элементов
+        [menuSpan, menuHamb].forEach(function(elem) {
+            elem.addEventListener("click", function() {
+                transformBtn();
+                isClick();
+                outClick();
+            });
         });
 
         function transformBtn() {
@@ -22,8 +27,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
         function outClick() {
             document.onclick = function(e) {
-                if (e.target.className != "menu__panel") {
+                if (e.target.className != "menu__panel" && 
+                    e.target.className != "hamb" &&
+                    e.target.className != "menu__hamb") {
                     menuPanel.classList.remove("is-click");
+                    menuSpan.classList.remove("click");
                 }
             }
         }
