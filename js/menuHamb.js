459986a3b9df27efee21b7130c98e72a0014c60b
Popil Roman <popil.roman23@mail.ru>
2023-02-21 21:34:07 +0500

    Исправил кнопку меню

--- js/menuHamb.js
+++ js/menuHamb.js
@@ -3,18 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
     // Функция, которая проверяет загрузку всего контента страницы
     window.addEventListener("load", function() {
 
-        const  menuHamb = this.document.querySelector(".menu__hamb");
-        const menuSpan = this.document.querySelector(".hamb");
+        const menuHamb = this.document.querySelector(".menu__hamb");
+        const menuSpan = this.document.getElementById("hamb");
         const menuPanel = this.document.querySelector(".menu__panel");
 
 
-        //Массив для события по одному из элементов
-        [menuSpan, menuHamb].forEach(function(elem) {
-            elem.addEventListener("click", function() {
-                transformBtn();
-                isClick();
-                outClick();
-            });
+        
+        menuHamb.addEventListener("click", function() {
+            transformBtn();
+            isClick();
+            outClick();
         });
 
         function transformBtn() {
@@ -28,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
         function outClick() {
             document.onclick = function(e) {
                 if (e.target.className != "menu__panel" && 
-                    e.target.className != "hamb" &&
+                    e.target.id != "hamb" &&
                     e.target.className != "menu__hamb") {
                     menuPanel.classList.remove("is-click");
                     menuSpan.classList.remove("click");
